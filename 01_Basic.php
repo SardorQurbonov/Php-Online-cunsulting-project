db22a8120731d963564e51c8f7e99c74fe9ddf46
SardorQurbonov <85383677+SardorQurbonov@users.noreply.github.com>
2022-07-28 10:04:21 +0500

    Phpproject Githubga qo'shildi

---
+++ 01_Basic.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+    This is my firs php website
+    <?php
+    define('PI3', 3.14);
+    echo "Hello world and this is printed using php";
+
+    // secret algoritm
+
+    /*
+        This
+        is
+        a
+        multi
+        line
+        comment
+        */
+
+        $variable1 = 5;
+        $variable2 = 2;
+        echo $variable1;
+        echo $variable2;
+
+        Echo $variable1 + $variable2;
+
+        // operators in PHP
+        // Arithmetic operators
+        echo "<br/>";
+        echo "The value of variable + variable2 is";
+        echo $variable1 + $variable2;
+        echo "<br/>";
+        echo "The value of variable - variable2 is";
+        echo $variable1 - $variable2;
+        echo "<br/>";
+        echo "The value of variable * variable2 is";
+        echo $variable1 * $variable2;
+        echo "<br/>";
+        echo "The value of variable / variable2 is";
+        echo $variable1 / $variable2;
+        echo "<br/>";
+        //Assignment operators
+        $newVar = $variable1;
+        // $newVar += 1;
+        // $newVar -= 1;
+        // $newVar *= 2;
+        $newVar /= 2;
+        echo "The value of new variable is";
+        echo $newVar;
+        echo "<br/>";
+        //Comparison Operators
+        // echo "<h1> Comparison Operators </h1>";
+        echo "The value of 1==4 is";
+        echo var_dump(1==4);
+        echo "<br/>";
+
+        echo "The value of 1!=4 is";
+        echo var_dump(1!=4);
+        echo "<br/>";
+
+        echo "The value of 1>=4 is";
+        echo var_dump(1>=4);
+        echo "<br/>";
+
+        echo "The value of 1<=4 is";
+        echo var_dump(1<=4);
+        echo "<br/>";
+        //Increment/Decrement operators
+    //    echo $variable1++;
+    //    echo $variable1--;
+    //    echo ++$variable1;
+       echo --$variable1;
+       echo "<br/>";
+       echo $variable1;
+        // Logical Operator
+    // and (&&)
+    // or (||)
+    // xor
+    // !
+
+    // $myVar = (true and  true);
+    // $myVar = (false and  true);
+    // $myVar = (false and  false);
+    // $myVar = (true and false);
+    // $myVar = (true or false);
+
+    //  $myVar = (true xor  true);
+    // $myVar = (false and  true);
+    // $myVar = (false xor  false);
+    $myVar = (true and false);
+    echo "<br/>";   
+    echo var_dump($myVar);
+    ?>
+
+    <?php
+        // Data types in php
+        // 1.String
+        // 2.Integer
+        // 3.float
+        // 4.Boolean
+        // 5.Array
+        // 6.Object
+        echo "<br> Data types <br>";
+        $var = "This is a string";
+        echo var_dump($var);
+        echo "<br>";
+
+        $var = 67;
+        echo var_dump($var);
+        echo "<br>";
+
+        $var = 67.1;
+        echo var_dump($var);
+        echo "<br>";
+
+        $var = true;
+        echo var_dump($var);
+        echo "<br>";
+        echo PI3;
+        ?>
+
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+    //Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection succes
+    if(!$con){
+        die("connection to this database failed due to" .
+        mysqli_connect_error());
+    }
+    // echo "Succes connecting to the db";
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age',
+    '$gender', '$email', '$phone', '$desc', CURRENT_TIMESTAMP());";
+    // echo $sql;
+
+    // Execute the query
+    if($con ->query($sql) == true){
+        // echo "Seccessfully inserted";
+
+        // flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+ 
+    }
+    // Close the database connection
+    $con->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&
+    display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="IIT kharagpur">
+    <div class="container">
+        <h1>Welcome to IIT kharagpur US Trip form</h1>
+        <p>Enter your details and submit this form to confirm your 
+            participation in the trip </p>
+            <?php
+            if($insert == true){
+          echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining coming
+                for the US trip</p>";
+            }
+            ?>
+            <form action="index.php" method="post">
+                <input type="text" name="name" id="name" placeholder="Enter your name">
+                <input type="text" name="age" id="age" placeholder="Enter your age">
+                <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+                <input type="email" name="email" id="email" placeholder="Enter your email">
+                <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+                <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+                <button class="btn">Submit</button>
+            </form>
+    </div>
+    <script src="index.js"></script>
+</body>
+</html>
